47f9ab0999ab07669edfc0fe9c115cfab73b3f31
Dmitriy <gun.dimonchik@gmail.com>
2019-02-08 23:33:06 +0200

    по видео задание 3

---
+++ lesson 2/js.js
@@ -0,0 +1,52 @@
+/*let num = 50;
+
+if (num < 49) {
+ console.log("Неверно!")
+} else if (num > 100){
+    console.log("Много!")
+} else {
+    console.log("Верно!")
+};
+
+(num == 50) ? console.log('Верно!') : console.log('Неверно');
+
+
+switch (num) {
+    case num < 49:
+        console.log("Неверно!");
+        break;
+    case num >100:
+        console.log("много!");
+        break;
+    case num >100:
+        console.log("Все еще много!");
+        break;
+    case num >100:
+        console.log("Верно!");
+        break;
+    default:
+        console.log("Что-то пошло не так!");
+        break;
+}
+
+let num = 50;
+//while (num < 55) {
+//   console.log(num);
+//   num++
+//}
+
+do {
+    console.log(num);
+    num++
+}
+while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if(i == 6) {
+        // break - прерывание цыкла полностью
+        continue // - прерывает только это значение
+    }
+    console.log(i)
+}console.log(let y = 1; let x = y = 2);
+*/
+alert( 0 || "" || 2 || undefined || true || falsе );
