66c68cd23dd8c07448b441c06d8cfcc94085f6f2
Dmitriy <gun.dimonchik@gmail.com>
2019-02-11 00:11:40 +0200

    Поправил и переименновал папки с заданиями

--- task 2/script.js
+++ task 2/script.js
@@ -1,3 +1,6 @@
+"use strict"
+
+
 let money = +prompt("Ваш бюджет на месяц", ""),
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
@@ -14,8 +17,7 @@ for (let = i; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
         b = prompt("Во сколько обойдется?", "");
 
-    if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
-        && a != '' && b != '' && a.length < 50) {
+    if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
         console.log("done");
         appData.expenses[a] = b;
     } else {
