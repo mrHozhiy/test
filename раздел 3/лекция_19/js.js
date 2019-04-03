f44848916feb53beea138d514b8591124675bbb1
Dmitriy <gun.dimonchik@gmail.com>
2019-04-03 21:10:29 +0300

    раздел 3

---
+++ раздел 3/лекция_19/js.js
@@ -0,0 +1,14 @@
+// let timeId = setTimeout(func, 3000);
+// clearTimeout(timeId);
+
+// let timeId = setInterval(func, 3000);
+// clearTimeout(timeId);
+
+// function sayHello() {
+//     console.log('Hello World!');
+// };
+
+// let timerId = setTimeout(function log() {
+//     console.log("Hello");
+//     setTimeout(log, 2000);
+// });
