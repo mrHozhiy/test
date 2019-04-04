63176ad8b6df51a662bb8d424a10b77d68eebff7
Dmitriy <gun.dimonchik@gmail.com>
2019-04-05 00:16:21 +0300

    параметры документа и окна браузера

---
+++ раздел 3/лекция_21/js.js
@@ -0,0 +1,38 @@
+let box = document.querySelector('.box'),
+    btn = document.querySelector('button');
+
+// let width = box.clientWidth, // ширина без скролла
+//     height = box.clientHeight; // высота без скролла
+
+// console.log(width);
+// console.log(height);
+
+
+// let width = box.offsetWidth, // ширина c скролла
+//     height = box.offsetHeight; // высота c скролла
+
+// console.log(width);
+// console.log(height);
+
+// let width = box.scrollWidth, // вся ширина c скролла
+//     height = box.scrollHeight; // вся высота c скролла
+
+// console.log(width);
+// console.log(height);
+
+
+
+let width = box.scrollWidth, // вся ширина c скролла
+    height = box.scrollHeight; // вся высота c скролла
+
+console.log(width);
+console.log(height);
+console.log(box.getBoundingClientRect().left);
+
+console.log(document.documentElement.clientWidth);
+console.log(document.documentElement.clientHeight);
+
+
+btn.addEventListener('click', function() {
+    box.style.height = box.scrollHeight + "px"
+});
