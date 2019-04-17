9da04be507495ae2c26ca44c4ea786e6c3f17f6e
Dmitriy <gun.dimonchik@gmail.com>
2019-04-18 00:19:06 +0300

    задание 14 п.1

--- раздел 4/yoga/js/script.js
+++ раздел 4/yoga/js/script.js
@@ -126,42 +126,61 @@ window.addEventListener('DOMContentLoaded', function() {
     };
     
     let form = document.querySelector('.main-form'),
+        formBottom = document.getElementById('form'),
         input = form.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
-
         statusMessage.classList.add('status');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        form.appendChild(statusMessage);
 
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    function sendForm(elem) {
+        elem.addEventListener('submit', function(e) {
+            e.preventDefault();
+            elem.appendChild(statusMessage);
+            let formData = new FormData(form);
 
-        let formData = new FormData(form);
+            function postData(data) {
 
-        let obj = {};
-        formData.forEach(function(value, key) {
-            obj[key] = value;
-        });
-        let json = JSON.stringify(obj);
+                return new Promise(function(resolve,reject){
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
-        request.send(json);
+                    request.onreadystatechange = function() {
+                        if (request.readyState < 4) {
+                            resolve()
+                        } else if (request.readyState === 4) {
+                            if (request.status == 200 && request.status < 300) {
+                                resolve()
+                            }
+                        } else {
+                            reject()
+                        }
+                    }
 
-        request.addEventListener('readystatechange', function() {
-            if (request.readyState < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if (request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success;
-            } else {
-                statusMessage.innerHTML = message.failure;
-            } 
-        });
+                    request.send(data);
+                });
+            }//end posData
 
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
-        }
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            }
 
+            postData(formData)
+                .then(()=> statusMessage.innerHTML = message.loading)
+                .then(()=> {
+                    thanksModal.style.display ='block';
+                    mainModal.style.display = 'none';
+                    statusMessage.innerHTML = '';
+                })
+                .catch(()=> statusMessage.innerHTML = message.failure)
+                .then(clearInput)
     });
+
+        
+    }
+    sendForm(form);
+    sendForm(formBottom);
+
 });
